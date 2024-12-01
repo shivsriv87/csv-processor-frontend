caa6101eb9b0081735605b949a79c01a6601a73d
Shivang Srivastava <shiv.sriv87@gmail.com>
2024-12-01 18:40:38 +0530

    bugs resolving

--- src/components/CsvUploader.jsx
+++ src/components/CsvUploader.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import DataTable from './DataTable';
+import DataTable from './Datatable';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
